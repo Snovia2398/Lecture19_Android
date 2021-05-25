1c98dee79962929b680059df0b80e56f8abf76ac
Snovia2398 <snovia2398@gmail.com>
2021-05-26 00:05:47 +0500

    Fragment2 completed

--- Lecture19/app/src/main/java/com/snovia/fragments/FragmentTwo.java
+++ Lecture19/app/src/main/java/com/snovia/fragments/FragmentTwo.java
@@ -7,17 +7,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FragmentTwo extends Fragment {
 
     public FragmentTwo() {
         // Required empty public constructor
     }
-    
+
+    Button button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false);
+       
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+
+        button = view.findViewById(R.id.btn_frgm2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Fragment Two Button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 }
